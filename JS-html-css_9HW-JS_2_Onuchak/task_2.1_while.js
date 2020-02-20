868b747447edbf3ccbd55fa3e5468d3a4ee1f997
Yaroslav Onuchak <51050378+YaroslavOnuchak@users.noreply.github.com>
2020-02-20 13:01:57 +0200

    Update task_2.1_while.js

--- JS-html-css_9HW-JS_2_Onuchak/task_2.1_while.js
+++ JS-html-css_9HW-JS_2_Onuchak/task_2.1_while.js
@@ -5,6 +5,7 @@ while (count >= 0 && pass != enterPass) {
     enterPass = prompt('enter password')
     if (enterPass == pass) {
         alert('Запрошуємо на сайт');
+break;
     } else {
         alert(`у вас залишилося ${count} спроб`);
     }
