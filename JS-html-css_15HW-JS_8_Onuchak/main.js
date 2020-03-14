11147a47be396e783905704cda2b6dab96a8e771
Yaroslav Onuchak <onuchak.j@gmail.com>
2020-03-14 09:29:09 +0200

    Add files via upload

--- JS-html-css_15HW-JS_8_Onuchak/main.js
+++ JS-html-css_15HW-JS_8_Onuchak/main.js
@@ -39,55 +39,125 @@ let stopWatBox = queSr('.stopwatch')
 stopWatBox.innerHTML = `<h3>${h} : ${min} : ${s} : ${ms}</h3>`
 
 function stopWatch() {
- stopWatBox.innerHTML = `<h3>${h} : ${min} : ${s} : ${ms}</h3>`
-  
- ms++;
-        if (ms < 10) {
-            ms = '00' + ms
-        } else if (ms < 100) {
-            ms = '0' + ms
-        }
-        // console.log(ms)
-        stopWatBox.innerHTML = `<h3>${h} : ${min} : ${s} : ${ms}</h3>`
-        if (ms === 100) {
-            s++;
-            if (s < 10) s ='0' + s;
-            ms = 0;
-            stopWatBox.innerHTML = `<h3>${h} : ${min} : ${s} : ${ms}</h3>`
-            if (s === 60) {
-                min++;
-                if (min < 10) min ='0' + min;
-                s = 0;
-                stopWatBox.innerHTML = `<h3>${h} : ${min} : ${s} : ${ms}</h3>`
-                if (min === 60) {
-                    if (h < 10) h = '0'+ h
-                    h++;
+    stopWatBox.innerHTML = `<h3>${h} : ${min} : ${s} : ${ms}</h3>`
 
-                    m = 0;
-                    stopWatBox.innerHTML = `<h3>${h} : ${min} : ${s} : ${ms}</h3>`
-                }
+    ms++;
+    if (ms < 10) {
+        ms = '00' + ms
+    } else if (ms < 100) {
+        ms = '0' + ms
+    }
+    // console.log(ms)
+    stopWatBox.innerHTML = `<h3>${h} : ${min} : ${s} : ${ms}</h3>`
+    if (ms === 100) {
+        s++;
+        if (s < 10) s = '0' + s;
+        ms = 0;
+        stopWatBox.innerHTML = `<h3>${h} : ${min} : ${s} : ${ms}</h3>`
+        if (s === 60) {
+            min++;
+            if (min < 10) min = '0' + min;
+            s = 0;
+            stopWatBox.innerHTML = `<h3>${h} : ${min} : ${s} : ${ms}</h3>`
+            if (min === 60) {
+                if (h < 10) h = '0' + h
+                h++;
+
+                m = 0;
+                stopWatBox.innerHTML = `<h3>${h} : ${min} : ${s} : ${ms}</h3>`
             }
         }
-        
-        let  sW
+    }
+
+    let sW
 
 }
-let  sW
-queSr('#start').addEventListener('click', function(){
-    
-     sW = setInterval(stopWatch, 10)
+let sW
+queSr('#start').addEventListener('click', function () {
+
+    sW = setInterval(stopWatch, 10)
 })
-queSr('#stop').addEventListener('click', function(){
+queSr('#stop').addEventListener('click', function () {
     clearInterval(sW)
 })
-queSr('#loop').addEventListener('click', function(){
-    queSr('.loop').innerText =  stopWatBox.innerText 
+queSr('#loop').addEventListener('click', function () {
+    queSr('.loop').innerText = stopWatBox.innerText
 })
-queSr('#reset').addEventListener('click', function(){
-     ms = '000';
-     s = '00';
-     min = '00';
-     h = '000';
-     stopWatBox.innerHTML = `<h3>${h} : ${min} : ${s} : ${ms}</h3>`
+queSr('#reset').addEventListener('click', function () {
+    ms = '000';
+    s = '00';
+    min = '00';
+    h = '000';
+    stopWatBox.innerHTML = `<h3>${h} : ${min} : ${s} : ${ms}</h3>`
 })
- 
+
+
+
+
+
+
+let minTime = '00';
+let secTime = '00';
+
+let timeSet = queSr('.timer-set-number')
+let timeCaunt = queSr('.timer-caunt-number')
+timeSet.innerText = `${minTime}`
+
+queSr('#plus').addEventListener('click', function () {
+    minTime++
+    if (minTime < 10) minTime = '0' + minTime;
+    timeSet.innerText = `${minTime}`
+
+})
+queSr('#minus').addEventListener('click', function () {
+    if (minTime > 0) {
+        minTime--
+        if (minTime < 10) minTime = '0' + minTime;
+    }
+    timeSet.innerText = `${minTime}`
+})
+
+function timer() {
+    // timeCaunt.innerText = `${minTime} : ${secTime}`
+
+    timeCaunt.innerText = `${minTime} : ${secTime}`
+    console.log(secTime, minTime)
+    if (minTime == '00' && secTime == '00') {
+        timeCaunt.innerText = `${minTime} : ${secTime}`
+        clearInterval(sTimer);
+        timeSet.innerText = `${minTime}`
+        // alert('time is over')
+    } else {
+
+        if (secTime == '00') {
+            timeCaunt.innerText = `${minTime} : ${secTime}`
+            secTime = 59
+            minTime--;
+            if (minTime < 10) minTime = '0' + minTime
+            timeCaunt.innerText = `${minTime} : ${secTime}`
+
+        }
+        secTime--;
+
+        if (secTime < 10) secTime = '0' + secTime
+
+    }
+}
+
+let sTimer
+queSr('#startT').addEventListener('click', function () {
+
+    sTimer = setInterval(timer, 1000)
+})
+
+queSr('#stopT').addEventListener('click', function () {
+    clearInterval(sTimer)
+    timeSet.innerText = `00`
+})
+
+queSr('#resetT').addEventListener('click', function () {
+    minTime = '00'
+    secTime = '00'
+    timeCaunt.innerText = `${minTime} : ${secTime}`
+    timeSet.innerText = `${minTime}`
+})
